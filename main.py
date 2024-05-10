04079610fd0c11aa28d7555c1443a9bade540b07
Danyil Zlobin <112890351+Danemm99@users.noreply.github.com>
2024-05-11 01:13:58 +0300

    Update main.py (API was implemented).

---
+++ main.py
@@ -0,0 +1,25 @@
+import torch
+import shutil
+import os
+from fastapi import FastAPI, UploadFile
+
+
+app = FastAPI(
+    title='Get Building Damage'
+)
+
+
+@app.post("/upload_photo_of_building")
+def upload_photo_of_building(file: UploadFile):
+    with open(file.filename, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    img_path = file.filename
+
+    model = torch.hub.load('yolov5', 'custom', 'runs/train/exp/weights/yolov5s.pt', source='local', force_reload=True)
+    results = model(img_path)
+    results.show()
+
+    os.remove(img_path)
+
+    return {"status": "Photo was successfully analyzed."}
