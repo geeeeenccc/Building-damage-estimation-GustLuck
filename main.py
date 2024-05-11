287bd678e8b88b7bf547db8c2afbbc4653a7498d
Danyil Zlobin <112890351+Danemm99@users.noreply.github.com>
2024-05-11 18:26:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import torch
 import shutil
 import os
-from fastapi import FastAPI, UploadFile
+import uvicorn
+from fastapi import FastAPI, UploadFile, Request
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI(
     title="Calculate Building's Damage"
 )
+templates = Jinja2Templates(directory='templates')
 
 
 @app.post("/upload_photo_of_building/")
-def upload_photo_of_building(file: UploadFile):
+async def upload_photo_of_building(request: Request, file: UploadFile):
     with open(file.filename, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     img_path = file.filename
 
-    model = torch.hub.load('yolov5', 'custom', 'runs/train/exp/weights/yolov5s.pt', source='local', force_reload=True)
+    model = torch.hub.load('yolov5', 'custom', 'yolov5/runs/train/exp8/weights/best.pt', source='local', force_reload=True)
     results = model(img_path)
     results.show()
-
     os.remove(img_path)
 
-    return {"status": "Photo was successfully analyzed."}
+    return templates.TemplateResponse("results.html", {"request": request})
